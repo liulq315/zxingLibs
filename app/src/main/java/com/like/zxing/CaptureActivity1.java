9abe815351de8ee486e57d5bdaf545f21114aa37
liulq <mylike_45@163.com>
2017-12-06 16:55:49 +0800

    '添加暂停功能'

--- app/src/main/java/com/like/zxing/CaptureActivity1.java
+++ app/src/main/java/com/like/zxing/CaptureActivity1.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import org.loqs.zxing.activity.QRCodeFragment;
+import org.lqos.zxing.activity.QRCodeFragment;
 
 public class CaptureActivity1 extends AppCompatActivity {
     Button mLight;
@@ -42,11 +42,12 @@ public class CaptureActivity1 extends AppCompatActivity {
 
     public void ssButtonss(View view) {
         QRCodeFragment fragment = (QRCodeFragment) getFragmentManager().findFragmentByTag("QRCodeFragment");
-        fragment.photo();
+        fragment.getHandler().sendEmptyMessage(R.id.restart_pause);
     }
 
     public void ssopen(View view) {
         QRCodeFragment fragment = (QRCodeFragment) getFragmentManager().findFragmentByTag("QRCodeFragment");
         mLight.setText(fragment.lightOnAndOff() ? "关闭灯光" : "打开灯光");
+        fragment.getHandler().sendEmptyMessage(R.id.restart_restart);
     }
 }
--- app/src/main/java/com/like/zxing/MainActivity.java
+++ app/src/main/java/com/like/zxing/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.loqs.zxing.encoding.EncodingHandler;
+import org.lqos.zxing.encoding.EncodingHandler;
 
 
 public class MainActivity extends AppCompatActivity {
